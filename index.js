7b90b0a930310ebcd15d5a214efe2920e5103fbb
shenobyu <shenobyu@gmail.com>
2023-12-21 10:47:56 -0300

    ta pronto

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello")
+class heroi{
+    constructor(nome, idade, tipo, ataque){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+        this.ataque = ataque
+    }
+    escrever(){
+        console.log(`o heroi ${this.nome} de classe ${this.tipo} de idade ${this.idade} atacou usando ${this.ataque}`)
+    }
+}
+
+let combo = new heroi("naruto", 19, "mago", "magia")
+
+combo.escrever()
+
+
+
